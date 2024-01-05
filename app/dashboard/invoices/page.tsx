94bb27a08e66ae0b65d8c033578ddcbc8136134d
tdraad <tdraad@gmail.com>
2024-01-05 12:22:45 -0800

    update env

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -40,6 +40,7 @@ export default async function Page({
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
       </div>
+      <p>my update w env</p>
     </div>
   );
 }
